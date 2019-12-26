9ddb828da8580aa91aba0b5de1fd5d4f93415ce1
Gonzalo Caba <charly021@gmail.com>
2019-12-26 12:20:17 -0300

    setup

---
+++ src/test/java/com/hexsoft/athos/AthosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexsoft.athos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
