22688136572ede076ac159bb99bafc81850d3706
Gonzalo Caba <charly021@gmail.com>
2019-12-26 12:51:46 -0300

    setup

--- src/main/java/com/hexsoft/athos/AthosApplication.java
+++ src/main/java/com/hexsoft/athos/AthosApplication.java
@@ -2,6 +2,10 @@ package com.hexsoft.athos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class AthosApplication {
@@ -10,4 +14,15 @@ public class AthosApplication {
 		SpringApplication.run(AthosApplication.class, args);
 	}
 
+	// Enable CORS globally
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				//registry.addMapping("/panel/*").allowedOrigins("http://192.168.0.102:8080");
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
